6fd0312234b244adca59335b45d46d8a2a690412
ophelipecamilo <pfernandescamilo@gmail.com>
2022-12-13 23:10:38 -0300

    Create Treinando-Python.py

---
+++ Treinando-Python.py
@@ -0,0 +1,47 @@
+
+print('Olá, mundo!')
+print()
+print('Meu nome é Phelipe e tenho', 26, 'anos')
+print()
+print(7 + 5)
+print(7.2+ 4.8)
+print()
+
+lanchePreco = 10.9
+batataPreco = 5.9
+refriPreco = 3
+print('Valor total:',lanchePreco + batataPreco + refriPreco)
+print()
+lancheNome = input('Qual lanche você quer? ')
+print('Você pediu um:', lancheNome)
+print()
+print()
+# Exercício 1
+nomePrimeiro = 'Phelipe'
+nomeMeio = 'Fernandes'
+nomeUltimo = 'Camilo'
+print(nomePrimeiro, nomeMeio, nomeUltimo, ', seja muito bem vindo a DNC')
+print()
+# Exercício 2
+nomePrimeiro = input('QUal o seu nome?')
+nomeMeio = input('QUal o seu sobrenome?')
+nomeUltimo = input('QUal o seu último nome?')
+print(nomePrimeiro, nomeMeio, nomeUltimo, ', seja muito bem vindo a DNC')
+print()
+print()
+# Exercício 3
+cliente_nome = input('Qual o nome do cliente?')
+cliente_idade = input('Qual a idade do cliente?')
+print('O nome do clinete é:',cliente_nome)
+print('A idade do cliente é:',cliente_idade)
+print()
+print()
+# Exercício 4
+produtoValor = 25.30
+clientePremium = True
+print('O valor do produto é:', produtoValor)
+print('O tipo da variável produtoValor é:', type(produtoValor))
+print('O cliente é premium?',clientePremium)
+print('O tipo da variável clientePremiun é:', type(clientePremium))
+
+import this
